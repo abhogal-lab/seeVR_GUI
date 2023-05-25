225c5f532f417ceb66d26706af99eacf88d09fa5
abhogal <abhogal@LA018017.ds.umcutrecht.nl>
2023-05-25 14:13:30 +0200

    test

--- seeVR_set_path.m
+++ seeVR_set_path.m
@@ -3,12 +3,11 @@ function [seeVR elastix] = seeVR_set_variables(  )
 
 %% Specify the directories of the toolbox here
 seeVR     = 'E:\seeVR-main\'; %specify toolbox path with trailing slash
+seeVR = fullfile(seeVR);
 
-if ispc
-elastix   = [seeVR,'elastix\']; 
-else
-elastix   = [seeVR,'elastix/'];   
-end
+elastix   = 'C:\Users\abhogal\MATLAB\Projects\seeVR_GUI\functions\elastix'
+elastix   = fullfile(elastix);
+
 addpath(genpath(seeVR));
 
 % if you have standardized filenames from pre-processing steps, set them
